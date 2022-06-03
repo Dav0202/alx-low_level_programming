2d63177d42e2a54607845da8bcd0a6b932a0768a
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-03 16:13:22 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,13 @@ int main(void)
 	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
+		putchar(',');
+		putchar(' ');
 	}
 	if (i != 57)
 	{
-		putchar(',');
-		putchar(' ');
-	}	
+		putchar('')
+	}
 	putchar('\n');
 	return (0);
 }
