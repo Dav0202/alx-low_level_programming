959e959395c01116f9e0d4bd9d67bc49dc7bcbf3
Dave <davidbrain162@gmail.com>
2022-06-03 08:01:18 -0700

    20th commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
  * main - Prints a list of numbers
- * Description: prints the lowercase alphabet in reverse, followed by a new line.
  * Return: Always(0)
  */
 int main(void)
 {
 	char c = 'z';
-  
+
 	while (c >= 'a')
 	{
 		putchar(c);
