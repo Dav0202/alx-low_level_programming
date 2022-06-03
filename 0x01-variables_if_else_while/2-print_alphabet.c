3b7b3212b104493c822d66061f1b45e3f2a8b651
Dave <davidbrain162@gmail.com>
2022-06-03 05:47:11 -0700

    21st commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - alphabet in lowercase, followed by a new line
+ * Return: Always(0)
+ */
+int main(void)
+{
+	char charater = 'a';
+
+	for (charater = 'a'; charater <= 'z', charater++)
+	{
+		putchar(charater)'
+	}
+	putchar("\n");
+	return(0);`
+}
