b2791372c14da11b0f9ccef255a05cad335e9d83
Dave <davidbrain162@gmail.com>
2022-06-03 06:41:20 -0700

    21st commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /**
  * main - alphabet in lowercase and upper, followed by a new line.
- * Always return(0)
+ * Return: Always(0)
  */
 int main(void)
 {
 	char ch1 = 'a';
+
 	char ch2 = 'A';
+
 	for (ch1 = 'a'; ch1 <= 'z'; ch1++)
 	{
 		putchar(ch);
