70ccf74dae7d282034f71fd0eb90d203426c97c0
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-07 22:01:58 +0100

    Update 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add two numbers from input
+ * @a: parameter 1
+ * @b: parameter 2
+ * Description: adds numbers
+ * Return: Always return 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
