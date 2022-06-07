eb68a7419a0022b0cabba0c7369904a6b8289e8e
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-07 23:34:04 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * print_to_98 - prints numbers to 98
+ * @n: parameter
+ * Description: prints all num 
+ * Return: Always return 0
+ */
 void print_to_98(int n)
 {
 	int i;
