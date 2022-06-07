dae33995d0684e87a43f77b9389ac4c607abc4aa
Dave <davidbrain162@gmail.com>
2022-06-07 05:22:43 -0700

    23rd commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print - print alphabets
+ * Description: prints alphabets 10 times
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		char = 'a';
+
+		while (char <= 'z')
+		{
+			_putchar(char);
+			ch++;
+		}
+		n++;
+		_putchar('\n');
+	}
+}
