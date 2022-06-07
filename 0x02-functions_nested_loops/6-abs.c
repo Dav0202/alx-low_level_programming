c093f1b4db5be40b19805a71b71400cc9502a29b
Dave <davidbrain162@gmail.com>
2022-06-07 07:01:30 -0700

    24th commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - absolute
+ * Description: Returns absolute number of input
+ * Return: Always returns 0
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
