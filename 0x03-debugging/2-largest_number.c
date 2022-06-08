5efc05ec5ad8341b0695645368ad0c8e0f9d9229
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-08 14:18:56 +0100

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,13 +1,9 @@
 #include "holberton.h"
 /**
  * largest_number - returns the largest of 3 numbers
-
  * @a: first integer
-
  * @b: second integer
-
  * @c: third integer
-
  * Return: largest number
  */
 
