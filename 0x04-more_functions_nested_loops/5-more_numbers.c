f7688e348484dac0ca238ffae5bbbb0c62c47b9f
Dave <davidbrain162@gmail.com>
2022-06-09 07:11:26 -0700

    29th commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers
+ * Description: prints list of numbers 10 times
+ * Retuen: Always return 0
+ */
+void more_numbers(void)
+{
+	int i, n;
+
+	while (i <= 10)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 10)
+			{
+				_putchar(i / 10 + '0');
+			}
+			else
+			{
+				_putchar(i % 10 + '0');
+			}
+		}
+		_putchar('\n');
+
+		i++
+	}
+}
