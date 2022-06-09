602e920acf88a64dc3000a76228459c906451b6c
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-09 17:34:14 +0100

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,26 +1,47 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - program that prints out the highest prime
+ * print_number - print number
+ * @n: input value
+ * Description:  prints an integer
  * Return: Always (0)
  */
-int main(void)
+void print_number(int n)
 {
-	int n, i, d;
-  
-	d = 50829601;
-  
-	n = 150;
-  
-	for (i = 3; i <= n; i = i + 2)
+	if ((n < 0) && (n >= -9))
 	{
-		while (n % i == 0)
-		{
-			n = n / i;
-		}
+		_putchar('-');
+		_putchar((n * -1) + '0');
 	}
-	printf("%d", d);
-
-	printf("\n");
-
-	return (0);
+	else if (n <= -10)
+	{
+		_putchar('-');
+		_putchar(((n / 10) * -1) + '0');
+		_putchar(((n % 10)  * -1) + '0');
+	}
+	else if (n == 0)
+	{
+		_putchar(n + '0');
+	}
+	else if ((n > 0) && (n <= 9))
+	{
+		_putchar(n + '0');
+	}
+	else if ((n >= 10) && (n <= 99))
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if ((n >= 100) && (n <= 999))
+	{
+		_putchar((n / 100) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if ((n >= 1000) && (n <= 9999))
+	{
+		_putchar((n / 1000) + '0');
+		_putchar((n / 100) % 10 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar((n % 10) + '0');
+	}
 }
