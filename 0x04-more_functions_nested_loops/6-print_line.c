a310a041bf8542b5f7de561a676c5a8836f01c20
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-09 16:02:02 +0100

    Update 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - print line
+ * @n: parameter
+ * Description:  draws a straight line
+ * Return: Always return 0
+ */
+ 
+void print_line(int n)
+{
+	int i;
+  if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
