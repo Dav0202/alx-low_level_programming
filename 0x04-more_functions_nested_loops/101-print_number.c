45db27686dd2d0ea3d26e8b49b0d7b2033cd6f71
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-09 20:02:38 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -27,17 +27,4 @@ void print_number(int n)
 		_putchar((n / 10) + '0');
 		_putchar((n % 10) + '0');
 	}
-	else if ((n >= 100) && (n <= 999))
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if ((n >= 1000) && (n <= 9999))
-	{
-		_putchar((n / 1000) + '0');
-		_putchar((n / 100) % 10 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar((n % 10) + '0');
-	}
 }
