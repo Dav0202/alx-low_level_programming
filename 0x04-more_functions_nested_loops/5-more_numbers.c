0af04a63a6b963605279ced6009f0beafad573b7
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-09 21:06:34 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -18,8 +18,8 @@ void more_numbers(void)
 				_putchar((i % 10) + '0');
 				if (i > 10)
 				{
-				_putchar(i / 10 + '0');	
-				}	
+				_putchar(i / 10 + '0');
+				}
 			}
 		}
 		_putchar('\n');
