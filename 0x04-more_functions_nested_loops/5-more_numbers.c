a1e1a376c9b9988bcdf5a2df9582e10ed79a7fab
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-09 21:25:04 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,16 +9,16 @@ void more_numbers(void)
 {
 	int n, i;
 
-	for (n = 0; n <= 10; n++)
+	for (n = 0; n < 10; n++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			if (i <= 10)
+			if (i <= 9)
 			{	
-				_putchar(i % 10 + '0');
+				_putchar(i + '0');
 				if (i > 10)
 				{
-					_putchar((i / 10) + '0');
+					_putchar((i % 10) + '0');
 				}
 			}
 		}
