13f5c4dc96b1cfc551caa721343a78037484be40
Dave <davidbrain162@gmail.com>
2022-06-09 09:46:02 -0700

    30th commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_diagonal - prints diagonal stuff
- * @n: integer to set diagonal
+ * print_diagonal - prints diagonal
+ * @n: integer
+ * Return: Always returns 0
  */
-
 void print_diagonal(int n)
 {
 	int i;
@@ -12,7 +12,8 @@ void print_diagonal(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
-	} else
+	}
+	else
 	{
 		for (i = 0; i < n; i++)
 		{
