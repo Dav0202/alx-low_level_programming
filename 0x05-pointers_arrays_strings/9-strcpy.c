cdb8cf816fd9e21b5b1faab0c29b31f27c8577c7
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-13 22:45:04 +0100

    Update 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcpy - main function to copy
+ * @dest: destination to copy
+ * @src: src
+ * Return: Dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+  
+  int p;
+  
+  p = 0;
+  
+  while (str[i] != '\0')
+	{
+		p++;
+	}
+  
+	for (i = 0; i <= p; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
