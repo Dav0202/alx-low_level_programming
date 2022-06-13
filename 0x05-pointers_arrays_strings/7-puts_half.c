6d61dcfd79865e6d56876fcd30a0da8a371cab69
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-13 21:58:55 +0100

    Update 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * puts_half - print half
+ * @str: parameter
+ * Description: prints half of a string
+ * Return: Always return;
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	int n;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 1)
+	{
+		n = (i + 1) / 2;
+		while (str[n] != '\0')
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	else
+	{
+		n = (i / 2);
+		while (str[n] != '\0')
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	_putchar('\n');
+}
