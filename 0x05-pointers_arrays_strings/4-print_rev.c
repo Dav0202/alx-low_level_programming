167986ce8b397a92eab184345e779c7f12d9c1b0
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-13 20:50:57 +0100

    Update 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - print reverse
+ * @s: parameter
+ * Description: prints a string, in reverse
+ * Return: Always returns 0
+ */
+void print_rev(char *s)
+{
+	int len, i;
+
+	len = 0;
+  
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		len = i;
+	}
+	for (i = len; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
