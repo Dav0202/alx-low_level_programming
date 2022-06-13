34ee2aab7fbd4c0178f7cd4ddf298cb470d2707f
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-13 20:25:50 +0100

    Update 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns lenght of string
+ * @s: parameter
+ * Description: uses pointers
+ * Return: Always return 0
+ */
+ 
+int _strlen(char *s)
+{
+	int i;
+  
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		continue;
+	}
+	return (i);
+}
