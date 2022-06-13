ab832378f0fb8ed92193e53cde64873c72f6bb66
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-13 21:46:45 +0100

    Update 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts2 - puts string twice
+ * @str: string to put twice
+ * Description: reverses a string.
+ * Return: Always return 0
+ */
+void puts2(char *str)
+{
+	int i;
+  
+  int n;
+  
+  n = 0;
+
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
