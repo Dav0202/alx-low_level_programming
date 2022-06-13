de2d245d00f040748348e4c570e214107d9a7abb
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-13 22:19:26 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,13 +8,13 @@
 void puts_half(char *str)
 {
 	int i;
-	
+
 	int p;
 
 	int n;
-	
+
 	i = 0;
-	
+
 	n = 0;
 
 	while (str[i] != '\0')
@@ -24,10 +24,17 @@ void puts_half(char *str)
 	if (i % 2 != 0)
 	{
 		n += 1;
+		for (p = i + n / 2; p < n; p++)   
+		{
+			_putchar(str[p]);
+		}
 	}
-	for (p = i + n / 2; p < n; p++)   
+	else
 	{
-		_putchar(str[p]);
+		for (p = i + n / 2; p < n; p++)   
+		{
+			_putchar(str[p]);
+		}
 	}
 	_putchar('\n');
 }
