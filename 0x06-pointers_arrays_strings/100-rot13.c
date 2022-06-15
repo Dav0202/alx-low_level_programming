b4afda79decbd1404266d2f961b6879032fc3473
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-15 13:24:16 +0100

    Update 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * rot13 - encode string
+ * @str: input parameter
+ * Description: encodes a string using rot13
+ * Return: alway
+ */
+char *rot13(char *s)
+{
+	char real[52] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+
+	char rot[52] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+  
+	int i;
+
+	int j = 0;
+  
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; real[j] != '\0'; j++)
+		{
+			if (s[i] == real[j])
+			{
+				s[i] = rot[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
