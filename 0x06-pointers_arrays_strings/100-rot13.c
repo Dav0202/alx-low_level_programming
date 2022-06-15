f601c2ec5b888d4f95af088c4fb1df6bfbcb822b
Dave <davidbrain162@gmail.com>
2022-06-15 05:27:39 -0700

    33rd commit|

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,11 +10,11 @@ char *rot13(char *s)
 	char real[52] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
 
 	char rot[52] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
-  
+
 	int i;
 
 	int j = 0;
-  
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; real[j] != '\0'; j++)
