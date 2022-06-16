17b98559fae607e1cc361c9333d4f609e41086df
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-16 22:14:16 +0100

    Update 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_number - encode string
+ * @n: input parameter
+ * Description: print number
+ * Return: always returns 0
+ */
+void print_number(int n)
+{
+	unsigned int i, x, count;
+  
+	if (n < 0)
+	{
+		i = -n;
+		_putchar(45);
+	}
+	else
+	{
+		i = n;
+	}
+	x = i;
+  
+	count = 1;
+  
+	while (x > 9)
+	{
+		x /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((i / count) % 10) + 48);
+	}
+}
