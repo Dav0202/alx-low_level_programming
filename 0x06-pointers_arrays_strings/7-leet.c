bcc3d01b44898eda929d04f52e2655d7d6cc7bf2
Dave <davidbrain162@gmail.com>
2022-06-16 02:50:39 -0700

    33rd commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * leet - encode string
- * @str: input string
+ * @s: input string
  * Description: encodes a string into 1337
  * Return: always (0)
  */
@@ -10,6 +10,7 @@ char *leet(char *s)
 	char Arr[] = "a4A4e3E3o0O0t7T7l1L1";
 	int i;
 	int j;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; Arr[j] != '\0'; j++)
