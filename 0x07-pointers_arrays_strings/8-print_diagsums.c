1f915bc0ac684a753c4291d67b8aae07663c9ef2
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-20 21:06:16 +0100

    Update 8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - print diagram sum
+ * @a: input parameter
+ * @size: input parameter
+ * Description: prints the sum of diagonals of square matrix
+ * Return: always 0
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, i1, j2;
+  
+	s1 = 0;
+
+	s2 = 0;
+  
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+			{
+				i1 += *((a + i * size) + j);
+			}
+			if ((i + j) == (size - 1))
+			{
+				j2 += *((a + i * size) + j);
+			}
+		}
+	}
+	printf("%i, %i\n", s1, s2);
+}
