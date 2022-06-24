3f4f56d4439df2354bc696fbb545eb5009d2373e
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-24 23:48:46 +0100

    Update 2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,1 +1,25 @@
-
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - locates a character
+ * @s: input parameter 1
+ * @c: input parameter 2
+ * Description: locates a character in a string
+ * Return: returns s or returns NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (!c)
+	{
+		return (s);
+	}
+	return (NULL);
+}
