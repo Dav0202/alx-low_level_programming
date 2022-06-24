99e3199e983c387585f8e2ccc7517d58490eb66c
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-24 23:47:56 +0100

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,1 +1,29 @@
+#include "main.h"
+/**
+ * _atoi - converts strings to int
+ * @s: first parameter
+ * Description: takes all types of char
+ * Return: 0 for success
+ */
+int _atoi(char *s) 
+{ 
+	int r, j, i;
 
+	r = 0;
+
+	j = 1; 
+
+	i = 0; 
+
+	if (s[0] == '-') { 
+
+		j = -1; 
+
+		i++; 
+	} 
+	for (; s[i] != '\0'; ++i)
+	{
+		r = r * 10 + s[i] - '0'; 
+	}
+	return (j * r); 
+}
