1a47a419639054a0852d0bebdb576c564ea33673
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-24 21:18:22 +0100

    Create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - multiplies two arguments and prints result
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if erroe doesnt exist
+ */
+int main(int argc, char *argv[])
+{
+	int i, first, second, result;
+
+	if (argc != 3)
+	{
+		return (printf("Error\n"), 1);
+	}
+	for (i = 0; i < argc; i++)
+	{
+		first = atoi(argv[1]);
+		second = atoi(argv[2]);
+	}
+	result = first_num * sec_num;
+	printf("%d\n", result);
+	return (0);
+}
