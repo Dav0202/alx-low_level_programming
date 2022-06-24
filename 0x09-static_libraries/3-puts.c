7e22f77887405a6dd8f740b16b651118c5ea65a1
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-24 23:52:11 +0100

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,1 +1,16 @@
-
+#include "main.h"
+/**
+ * _puts - put string
+ * @str: parameter
+ * Description: prints a string
+ * Return: Always Returns 0
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
