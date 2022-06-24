fcb85581d5f1cb88969478fd1fd2ba1484139b88
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-24 23:57:30 +0100

    Update 6-abs.c

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,1 +1,12 @@
-
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - absolute
+ * @n: Integer
+ * Description: Returns absolute number of input
+ * Return: Always returns 0
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
