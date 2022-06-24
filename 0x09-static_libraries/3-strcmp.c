77eeb142810249b1c4eb7aacdd1b6a905f8a6303
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-24 23:52:41 +0100

    Update 3-strcmp.c

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,1 +1,18 @@
+#include "main.h"
+/**
+ * _strcmp - compare strings
+ * @s1: first parameter
+ * @s2: second parameter
+ * Description: the function compares two strings
+ * Return: character count
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
 
+	while (s1[i] - s2[i] == 0 && s1[i] != '\0')
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
