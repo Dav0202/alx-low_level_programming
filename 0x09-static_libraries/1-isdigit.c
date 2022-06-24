782e2c15bde30b780d0f2eba77d2314b23017181
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-24 23:33:27 +0100

    Update 1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,1 +1,12 @@
+#include "main.h"
 
+/**
+ * _isdigit - determine if digit
+ * @n: value to determine
+ * Return: Returns zero or one
+ */
+
+int _isdigit(int n)
+{
+	return (n >= '0' && n <= '9');
+}
