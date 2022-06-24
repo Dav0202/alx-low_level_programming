e96d8ec6c438db0b47d0565add27fab513be842d
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-24 21:24:31 +0100

    Create 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds all positive numbers and then prints it
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if sucessful
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	res = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (i > 0)
+		{
+			if (atoi(argv[i]) == 0)
+			{
+				return (printf("Error\n"), 1);
+			}
+			j += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", j);
+	return (0);
+}
