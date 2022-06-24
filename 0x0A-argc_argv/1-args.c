13f1c25ec4002096e364d745c47a7e0094b9e930
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-24 21:13:19 +0100

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define UNUSED(x) (void)(x)
+/**
+ * main - prints number of arguments
+ * @argc: first parameter
+ * @argv: second parameter
+ * Description: prints number of arguments
+ * Return: 0 for code success
+ */
+int main(int argc, char **argv)
+{
+	int sum;
+  
+  UNUSED(argv);
+  
+	for (sum = 0; sum < argc; sum++)
+	{
+	}
+	printf("%d\n", (sum - 1));
+	return (0);
+}
