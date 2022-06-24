4be8497cd9a0c2205c8a87d661d687eae3e2f564
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-24 23:49:31 +0100

    Update 2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,1 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - returns lenght of string
+ * @s: parameter
+ * Description: uses pointers
+ * Return: Always return 0
+ */
+int _strlen(char *s)
+{
+	int i;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return (i);
+}
