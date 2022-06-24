c1100bc62b3100f97a65bd5b3c1ab19e339d2029
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-24 23:31:39 +0100

    Update 0-strcat.c

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,1 +1,25 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: first parameter
+ * @src: second parameter
+ * Description: appends the src string to the dest string
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, n;
 
+	n = 0;
+
+	while (dest[n] != '\0')
+	{
+		n++;
+	}
+	for (i = 0; src[i] != '\0'; i++, n++)
+	{
+		dest[n] = src[i];
+	}
+	dest[n] = '\0';
+	return (dest);
+}
