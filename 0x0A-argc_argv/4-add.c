f565016b8b191b186a44f682fd3d97270e7caf60
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-25 01:05:49 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,23 +11,18 @@ int main(int argc, char *argv[])
 	int i, j;
 
 	j = 0;
-	
-	if (argc > 1)
+
+	for (i = 0; i < argc; i++)
 	{
-		for (i = 0; i < argc; i++)
+		if (i > 0)
 		{
-			if (i > 0)
+			if (atoi(argv[i]) == 0)
 			{
-				if (atoi(argv[i]) == 0)
-				{
-					return (printf("Error\n"), 1);
-				}
-				j += atoi(argv[i]);
+				return (printf("Error\n"), 1);
 			}
+			j += atoi(argv[i]);
 		}
-		printf("%d\n", j);
-		return (0);
 	}
-	printf("%d\n", 0);
-	return (1);
+	printf("%d\n", j);
+	return (0);
 }
