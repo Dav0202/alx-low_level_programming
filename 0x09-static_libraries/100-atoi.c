46ada55b116a218f303c9fb94221ad7b9976695a
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-25 02:33:02 +0100

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -5,25 +5,26 @@
  * Description: takes all types of char
  * Return: 0 for success
  */
-int _atoi(char *s) 
-{ 
+int _atoi(char *s)
+{
 	int r, j, i;
 
 	r = 0;
 
-	j = 1; 
+	j = 1;
 
-	i = 0; 
+	i = 0;
 
-	if (s[0] == '-') { 
+	if (s[0] == '-')
+	{ 
 
 		j = -1; 
 
 		i++; 
-	} 
+	}
 	for (; s[i] != '\0'; ++i)
 	{
-		r = r * 10 + s[i] - '0'; 
+		r = r * 10 + s[i] - '0';
 	}
-	return (j * r); 
+	return (j * r);
 }
