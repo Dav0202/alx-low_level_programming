66290ee367f0dfb39e9fa73bf2c00176893e71d5
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-27 13:36:03 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,3 +9,20 @@
  * Return: a pointer to the array, or NULL if it fails
  */
  char *create_array(unsigned int size, char c);
+{
+ unsigned int i;
+ 
+ char *arr
+ 
+ ar = (char *) malloc(size);
+ 
+ if (size == 0 || ar == NULL)
+ {
+  return (NULL);
+ }
+ for (i = 0; i < size; i++)
+ {
+  ar[i] = c;
+ }
+ return (ar);
+}
