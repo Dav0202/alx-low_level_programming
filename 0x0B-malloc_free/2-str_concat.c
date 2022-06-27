8f6149ae65eb4fdce603b90a85ebdaafe2df30e7
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-27 14:13:22 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,1 +1,16 @@
-
+char *str_concat(char *s1, char *s2);
+{
+  unsigned int i;
+  int n;
+  char *ar
+ 
+  ar = (char *) malloc(n * sizeof(char));
+  if (ar == NULL || char == 0)
+  {
+        return (NULL);
+  }
+  else
+  {
+    for (i = 0; i < 
+  }
+}
