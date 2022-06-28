ca6dbf21d341f59e48b1f1c15cee9dbdc914084e
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-28 12:48:26 +0100

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,6 +21,10 @@ int **alloc_grid(int width, int height)
 	}
 	ar = (int **) malloc(height * sizeof(int *));
 
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
 		ar[i] = malloc(width * sizeof(int));
@@ -39,10 +43,6 @@ int **alloc_grid(int width, int height)
 		{
 			ar[i][j] = 0;
 		}
-	}
-	if (ar == NULL)
-	{
-		return (NULL);
 	}
 	return (ar);
 }
