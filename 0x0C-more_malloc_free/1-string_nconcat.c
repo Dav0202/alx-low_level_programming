67c4b84476747e84f11f4cf85542f5c9769b5221
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-06-30 21:38:32 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,37 +12,43 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ar;
 
-	unsigned int t, i, j;
-
-	t = 0;
+	unsigned int t1, t2, i;
 
 	if (s1 == NULL)
 	{
+		t1 = 0;
 		s1 = "";
+	}
+	for (t1 = 0; s1[t1]; ++t1)
+	{
 	}
 	if (s2 == NULL)
 	{
+		t2 = 0;
 		s2 = "";
 	}
-	while (s1[t] != '\0')
+	for (t2 = 0; s2[t2]; ++t2)
 	{
-		t++;
 	}
-	ar = malloc((t + n + 1) * sizeof(char));
+	if (t2 > n)
+	{
+		t2 = n;
+	}
+	ar = malloc((t1 + t2 + 1) * sizeof(char));
 
 	if (ar == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; i < t1; i++)
 	{
 		ar[i] = s1[i];
 	}
-	for (j = 0; s2[j] != '\0' && j < n; j++,i++)
+	for (i = 0; i < t2; i++)
 	{
-		ar[i] = s2[j];
+		ar[t1 + i] = s2[i];
 	}
-	ar[i] = '\0';
-	printf("%d",i);
+	ar[t1 + t2] = '\0';
+	printf("%d", t1+t2);
 	return (ar);
 }
