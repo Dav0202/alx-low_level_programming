75f26547d52cc2d54f426bad09fda6c34f1b6175
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-07-04 19:25:57 +0100

    Create 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints name of the file
+ * Description: prints name of file
+ * Return: Always return 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
