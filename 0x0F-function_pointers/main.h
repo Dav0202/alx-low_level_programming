930424315086cc7121744deb4f4c06351c052632
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-07-06 22:20:20 +0100

    Update main.h

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,5 +1,5 @@
-#ifndef _function_pointers_
-#define _function_pointers_
+#ifndef _main_
+#define _main_
 #include <stdio.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
