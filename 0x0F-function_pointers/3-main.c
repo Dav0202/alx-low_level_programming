4a440f511fb0cd67466fdc4e7e3e061fca4ddecc
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-07-06 23:20:28 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,15 +16,12 @@ int main(int argc, char *argv[])
 		printf("Error\n"), exit(98);
 	}
 	ptr = get_op_func(argv[2]);
-
 	if (!ptr)
 	{
 		printf("Error\n"), exit(99);
 	}
 	a = atoi(argv[1]);
-
 	b = atoi(argv[3]);
-
 	printf("%i\n", ptr(a, b));
 	return (1);
 }
