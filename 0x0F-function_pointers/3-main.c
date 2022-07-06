c99ec5efb524f617f5641aad1e792ade6904b532
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-07-06 21:43:34 +0100

    Create 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+/**
+ * main - performs simple operations
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 1 for success
+ */
+int main(int argc, char *argv[])
+{
+	register int a, b;
+	int (*ptr)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n"), exit(98);
+	}
+	ptr = get_op_func(argv[2]);
+	if (!ptr)
+	{
+		printf("Error\n"), exit(99);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	printf("%i\n", ptr(a, b));
+	return (1);
+}
