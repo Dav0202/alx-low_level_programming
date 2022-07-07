2ebad798e9bc0e9641542ea9337b0cd3c01a027a
Dave <davidbrain162@gmail.com>
2022-07-07 15:24:11 -0700

    55th commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -6,7 +6,6 @@
  * @separator: string to be printed between the strings
  * @n: number of strings passed to the function
  */
-
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
