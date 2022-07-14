e25918b79d437aead6ce29e90bff54cb013e572d
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-07-15 00:51:17 +0100

    Create 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * _strlen - counts the lenght of a string
+ * @str: string to be counted
+ * Return: len of a string
+ */
+int _strlen(const char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * add_node - adds a node at the beginning
+ * @head: first parameter
+ * @str: second parameter
+ * Return: node
+ */
+*add_node(list_t **head, const char *str)
+{
+	list_t *newnode;
+
+	newnode = malloc(sizeof(list_t));
+
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	newnode->str = strdup(str);
+	newnode->len = _strlen(str);
+	newnode->next = *head;
+
+	*head = newnode;
+
+	return (newnode);
+}
