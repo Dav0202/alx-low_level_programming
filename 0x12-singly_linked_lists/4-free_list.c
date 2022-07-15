e016a8a61999638b125bc3e7f1f2062f18ce0044
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-07-15 02:28:21 +0100

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 /**
  * free_list - Frees a list_t list.
- * @head: head pointer
+ * @h: head pointer
  */
-void free_list(list_t *head)
+void free_list(list_t *h)
 {
 	list_t *temp;
 
-	while (head)
+	while (h)
 	{
-		temp = head->next;
-		free(head->str);
-		free(head);
-		head = temp;
+		temp = h->next;
+		free(h->str);
+		free(h);
+		h = temp;
 	}
 }
