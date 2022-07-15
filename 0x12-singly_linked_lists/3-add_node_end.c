1be31283de3e2c3ba0b8c3c7b60e62a271ba1852
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-07-15 01:15:02 +0100

    Create 3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,61 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * _strlen - counts the lenght of a string
+ * @str: string to be counted
+ * Return: len of a string
+ */
+int _strlen(const char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * add_node_end - adds node at the end of a linked list
+ * @head: head pointer
+ * @str: input string
+ * Return: address of the new element.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	char *temp;
+	int len;
+	list_t *new, *last;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	temp = strdup(str);
+	if (str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->str = temp;
+	new->len = _strlen(str);
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+		{
+			last = last->next;
+		}
+		last->next = new;
+	}
+	return (*head);
+}
