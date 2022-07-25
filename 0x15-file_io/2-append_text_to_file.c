21be6174e91b6bd2d0ef59b28bd83c0d780b976b
Dave <davidbrain162@gmail.com>
2022-07-25 15:14:46 -0700

    67th commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * append_text_to_file - appends text at the end of a file
  * @filename: name of the file
  * @text_content: he NULL terminated string to add at the end of the file
  * Return: 1 on success and -1 on failure
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int _file, _write, text_len = 0;
