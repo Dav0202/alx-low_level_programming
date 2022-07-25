ce69305345cb9dd3a0e55b1c76b6af4e76e7f343
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-07-25 23:38:54 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,10 +14,6 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	_file = open(filename, O_WRONLY, O_APPEND);
-	if (_file == -1)
-	{
-		return (-1);
-	}
 	if (text_content)
 	{
 		while (text_content[text_len])
@@ -28,6 +24,17 @@ int append_text_to_file(const char *filename, char *text_content)
 		if (_write == -1)
 		{
 			return (-1);
+		}
+	}
+	else
+	{
+		if (_file == -1)
+		{
+			return (-1);
+		}
+		else
+		{
+			return (1);
 		}
 	}
 	close(_file);
