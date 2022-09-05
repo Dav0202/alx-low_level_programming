74b79d6d4d949004bbc5c90bf3a9101e1c803ea7
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2022-09-05 19:07:09 +0100

    Update functions.c

--- 0x18-dynamic_libraries/functions.c
+++ 0x18-dynamic_libraries/functions.c
@@ -354,3 +354,30 @@ unsigned int _strspn(char *s, char *accept)
 	}
 	return (i);
 }
+
+/**
+ * _atoi - converts strings to int
+ * @s: first parameter
+ * Description: takes all types of char
+ * Return: 0 for success
+ */
+int _atoi(char *s)
+{
+	int r, j, i;
+	r = 0;
+	j = 1;
+	i = 0;
+
+	if (s[0] == '-')
+	{
+
+		j = -1;
+
+		i++;
+	}
+	for (; s[i] != '\0'; ++i)
+	{
+		r = r * 10 + s[i] - '0';
+	}
+	return (j * r);
+}
